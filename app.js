194eb6d7cc3152716f1b72848df05c0a19a17bad
John Morgan <johnhmorgan@outlook.com>
2021-08-29 15:18:13 +0100

    adjusting code for cors

--- app.js
+++ app.js
@@ -2,12 +2,15 @@ const express = require("express");
 const cors = require("cors");
 
 const app = express();
+
+app.use(cors({origin: "https://managemyproperty.surge.sh", credentials: true}));
+app.use(express.json());
+
 const issueRoutes = require('./routes/issues');
 const userRoutes = require('./routes/users');
 
 
-app.use(cors({origin: "https://managemyproperty.surge.sh", credentials: true}));
-app.use(express.json());
+
 
   
 
