156f4ca481671ad1c8a3edc457ca8da22ae2c79a
john <johnhowardm@hotmail.com>
2021-11-07 18:49:21 +0000

    adding changes to solve CORS issue

--- app.js
+++ app.js
@@ -16,6 +16,7 @@ app.use("/users", userRoutes);
 
 app.use(function (req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Methods", "GET, POST, PUT");
     return next(new NotFoundError());
   });
 
